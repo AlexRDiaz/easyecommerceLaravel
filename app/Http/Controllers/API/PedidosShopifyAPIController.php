b990d94aecd28fa1d30d995df9f041ca2ad572ad
Joan <jeipige@gmail.com>
2023-08-25 15:49:32 -0500

    add sort method

--- app/Http/Controllers/API/PedidosShopifyAPIController.php
+++ app/Http/Controllers/API/PedidosShopifyAPIController.php
@@ -30,38 +30,111 @@ class PedidosShopifyAPIController extends Controller
         $endDate = $data['end'];
         $startDateFormatted = Carbon::createFromFormat('j/n/Y', $startDate)->format('Y-m-d');
         $endDateFormatted = Carbon::createFromFormat('j/n/Y', $endDate)->format('Y-m-d');
-        
-        $pageSize = $data['page_size']; 
-        $pageNumber = $data['page_number']; 
-        $searchTerm=$data['search'];
-        if($searchTerm!=""){
-        $filteFields=$data['or'];
-                $filteFields=$data['or'];
-        }else{
-            $filteFields=[];
+        $pageSize = $data['page_size'];
+        $pageNumber = $data['page_number'];
+        $searchTerm = $data['search'];
+        if ($searchTerm != "") {
+            $filteFields = $data['or']; // && SOLO QUITO  ((||)&&())
+        } else {
+            $filteFields = [];
         }
 
-        $pedidos = PedidosShopify::with('operadore.up_users')
+        // ! *************************************
+        $Map = $data['and'];
+        // ! *************************************
+        // ! ordenamiento ↓
+        $orderBy = null;
+        if (isset($data['sort'])) {
+            $sort = $data['sort'];
+            $sortParts = explode(':', $sort);
+            if (count($sortParts) === 2) {
+                $field = $sortParts[0];
+                $direction = strtoupper($sortParts[1]) === 'DESC' ? 'DESC' : 'ASC';
+                $orderBy = [$field => $direction];
+            }
+        }
+
+        // ! *************************************
+
+        $pedidos = PedidosShopify::with(['operadore.up_users'])
             ->with('transportadora')
             ->with('users.vendedores')
             ->with('novedades')
             ->with('pedidoFecha')
-            ->with('ruta')->with('subRuta')
+            ->with('ruta')
+            ->with('subRuta')
             ->whereRaw("STR_TO_DATE(fecha_entrega, '%e/%c/%Y') BETWEEN ? AND ?", [$startDateFormatted, $endDateFormatted])
+            
             ->where(function ($pedidos) use ($searchTerm, $filteFields) {
-
                 foreach ($filteFields as $field) {
-                    $pedidos->orWhere($field, 'LIKE', '%' . $searchTerm . '%');
+                    if (strpos($field, '.') !== false) {
+                        $relacion = substr($field, 0, strpos($field, '.'));
+                        $propiedad = substr($field, strpos($field, '.') + 1);
+                        $this->recursiveWhereHas($pedidos, $relacion, $propiedad, $searchTerm);
+                    } else {
+                        $pedidos->orWhere($field, 'LIKE', '%' . $searchTerm . '%');
+                    }
                 }
             })
-            ->paginate($pageSize, ['*'], 'page', $pageNumber);
-    
+            ->where((function ($pedidos) use ($Map) {
+                foreach ($Map as $condition) {
+                    foreach ($condition as $key => $valor) {
+                        if (strpos($key, '.') !== false) {
+                            $relacion = substr($key, 0, strpos($key, '.'));
+                            $propiedad = substr($key, strpos($key, '.') + 1);
+                            $this->recursiveWhereHas($pedidos, $relacion, $propiedad, $valor);
+                        } else {
+                            $pedidos->where($key, '=', $valor);
+                        }
 
-          
+                    }
+                }
+            }));
+            // ! Ordena
+            if ($orderBy !== null) {
+                $pedidos->orderBy(key($orderBy), reset($orderBy));
+            }
+            // ! **************************************************
+            $pedidos = $pedidos->paginate($pageSize, ['*'], 'page', $pageNumber);
+            // ! **************************************************
 
+            
 
         return response()->json($pedidos);
     }
+
+    private function recursiveWhereHas($query, $relation, $property, $searchTerm)
+    {
+        if($searchTerm=="null"){$searchTerm= null;}
+        if (strpos($property, '.') !== false) {
+            $nestedRelation = substr($property, 0, strpos($property, '.'));
+            $nestedProperty = substr($property, strpos($property, '.') + 1);
+
+            $query->whereHas($relation, function ($q) use ($nestedRelation, $nestedProperty, $searchTerm) {
+                $this->recursiveWhereHas($q, $nestedRelation, $nestedProperty, $searchTerm);
+            });
+        } else {
+            $query->whereHas($relation, function ($q) use ($property, $searchTerm) {
+                $q->where($property, 'LIKE', '%' . $searchTerm . '%');
+            });
+        }
+    }
+    private function recursiveWhere($query, $key, $property, $valor)
+    {
+        if($valor=="null"){$valor = null;}
+        if (strpos($property, '.') !== false) {
+            $nestedRelation = substr($property, 0, strpos($property, '.'));
+            $nestedProperty = substr($property, strpos($property, '.') + 1);
+            print($nestedProperty);
+            print($nestedRelation);
+            $query->whereHas($key, function ($query) use ($nestedRelation, $nestedProperty, $valor) {
+                $this->recursiveWhere($query, $nestedRelation, $nestedProperty, $valor);
+            });
+        } else {
+            $query->where($key, '=', $valor);
+        }
+    }
+
 
     public function store(Request $request)
     {
