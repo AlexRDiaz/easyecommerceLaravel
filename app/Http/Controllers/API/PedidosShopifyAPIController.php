36ae414b3dd6a8f31051523520e24037674c3d15
DianaC <dcarlosamam@gmail.com>
2023-08-25 12:19:48 -0500

    functions or-and

--- app/Http/Controllers/API/PedidosShopifyAPIController.php
+++ app/Http/Controllers/API/PedidosShopifyAPIController.php
@@ -30,16 +30,19 @@ class PedidosShopifyAPIController extends Controller
         $endDate = $data['end'];
         $startDateFormatted = Carbon::createFromFormat('j/n/Y', $startDate)->format('Y-m-d');
         $endDateFormatted = Carbon::createFromFormat('j/n/Y', $endDate)->format('Y-m-d');
-        
-        $pageSize = $data['page_size']; 
-        $pageNumber = $data['page_number']; 
-        $searchTerm=$data['search'];
-        if($searchTerm!=""){
-        $filteFields=$data['or'];
-                $filteFields=$data['or'];
-        }else{
-            $filteFields=[];
+        $pageSize = $data['page_size'];
+        $pageNumber = $data['page_number'];
+        $searchTerm = $data['search'];
+        if ($searchTerm != "") {
+            $filteFields = $data['or']; // && SOLO QUITO  ((||)&&())
+        } else {
+            $filteFields = [];
         }
+
+        $cont=0;
+        // ! *************
+        $Map = $data['and'];
+        // ! *************
 
         $pedidos = PedidosShopify::with('operadore.up_users')
             ->with('transportadora')
@@ -51,35 +54,90 @@ class PedidosShopifyAPIController extends Controller
             ->where(function ($pedidos) use ($searchTerm, $filteFields) {
 
                 foreach ($filteFields as $field) {
-                    $pedidos->orWhere($field, 'LIKE', '%' . $searchTerm . '%');
+                    if (strpos($field, '.') !== false) {
+                        $relacion = substr($field, 0, strpos($field, '.'));
+                        $propiedad = substr($field, strpos($field, '.') + 1);
+                        $this->recursiveWhereHas($pedidos, $relacion, $propiedad, $searchTerm);
+                    } else {
+                        $pedidos->orWhere($field, 'LIKE', '%' . $searchTerm . '%');
+                    }
                 }
             })
+
+            // ! no modificar el codigo arriba de esta linea ↑↑↑↑
+
+            ->where((function ($pedidos) use ($Map) {
+                foreach ($Map as $condition) {
+                    foreach ($condition as $key => $valor) {
+                        if (strpos($key, '.') !== false) {
+                            $relacion = substr($key, 0, strpos($key, '.'));
+                            $propiedad = substr($key, strpos($key, '.') + 1);
+                            $this->recursiveWhereHas($pedidos, $relacion, $propiedad, $valor);
+                        } else {
+                            $pedidos->where($key, '=', $valor);
+                        }
+
+                    }
+                }
+            }))
+            
+            // ! ******************
             ->paginate($pageSize, ['*'], 'page', $pageNumber);
-    
-
-          
-
-
         return response()->json($pedidos);
     }
 
-    // public function store(Request $request)
-    // {
-    //     $pedido = PedidosShopify::create($request->all());
-    //     return response()->json($pedido, Response::HTTP_CREATED);
-    // }
+    private function recursiveWhereHas($query, $relation, $property, $searchTerm)
+    {
+        if (strpos($property, '.') !== false) {
 
-    // public function update(Request $request, $id)
-    // {
-    //     $pedido = PedidosShopify::findOrFail($id);
-    //     $pedido->update($request->all());
-    //     return response()->json($pedido, Response::HTTP_OK);
-    // }
+            $nestedRelation = substr($property, 0, strpos($property, '.'));
+            $nestedProperty = substr($property, strpos($property, '.') + 1);
 
-    // public function destroy($id)
-    // {
-    //     $pedido = PedidosShopify::findOrFail($id);
-    //     $pedido->delete();
-    //     return response()->json(null, Response::HTTP_NO_CONTENT);
-    // }
+            $query->whereHas($relation, function ($q) use ($nestedRelation, $nestedProperty, $searchTerm) {
+                $this->recursiveWhereHas($q, $nestedRelation, $nestedProperty, $searchTerm);
+            });
+        } else {
+
+            $query->whereHas($relation, function ($q) use ($property, $searchTerm) {
+                $q->where($property, 'LIKE', '%' . $searchTerm . '%');
+            });
+        }
+    }
+
+
+    private function recursiveWhere($query, $key, $property, $valor)
+    {
+        print("entro en function recursiveWhere\n");
+
+        if (strpos($property, '.') !== false) {
+            $nestedRelation = substr($property, 0, strpos($property, '.'));
+            $nestedProperty = substr($property, strpos($property, '.') + 1);
+            $query->whereHas($key, function ($query) use ($nestedRelation, $nestedProperty, $valor) {
+                $this->recursiveWhere($query, $nestedRelation, $nestedProperty, $valor);
+            });
+        } else {
+            $query->where($key, '=', $valor);
+        }
+    }
+
+
+    public function store(Request $request)
+    {
+        $pedido = PedidosShopify::create($request->all());
+        return response()->json($pedido, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $pedido = PedidosShopify::findOrFail($id);
+        $pedido->update($request->all());
+        return response()->json($pedido, Response::HTTP_OK);
+    }
+
+    public function destroy($id)
+    {
+        $pedido = PedidosShopify::findOrFail($id);
+        $pedido->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
