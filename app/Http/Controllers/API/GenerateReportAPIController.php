8b40daf49545d55449a373ae8735f5c8ba23786a
AlexRDiaz <bugi2532@hotmail.com>
2023-09-16 10:00:34 -0500

    added excel library

--- app/Http/Controllers/API/GenerateReportAPIController.php
+++ app/Http/Controllers/API/GenerateReportAPIController.php
@@ -1,12 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\GenerateReport;
 use App\Models\PedidosShopify;
 use Carbon\Carbon;
+
+
+
+
+
 
 class GenerateReportAPIController extends Controller 
 {
@@ -73,114 +78,4 @@ class GenerateReportAPIController extends Controller
     }
 
 
-    private function recursiveWhereHas($query, $relation, $property, $searchTerm)
-    {
-        if ($searchTerm == "null") {
-            $searchTerm = null;
-        }
-        if (strpos($property, '.') !== false) {
-
-            $nestedRelation = substr($property, 0, strpos($property, '.'));
-            $nestedProperty = substr($property, strpos($property, '.') + 1);
-
-            $query->whereHas($relation, function ($q) use ($nestedRelation, $nestedProperty, $searchTerm) {
-                $this->recursiveWhereHas($q, $nestedRelation, $nestedProperty, $searchTerm);
-            });
-        } else {
-            $query->whereHas($relation, function ($q) use ($property, $searchTerm) {
-                $q->where($property, '=', $searchTerm);
-            });
-        }
-    }
 }
-
-//     public function generateReport(Request $request){
-
-//         $data = $request->json()->all();
-//         $startDate = $data['start'];
-//         $endDate = $data['end'];
-//         $startDateFormatted = Carbon::createFromFormat('j/n/Y', $startDate)->format('Y-m-d');
-//         $endDateFormatted = Carbon::createFromFormat('j/n/Y', $endDate)->format('Y-m-d');
-
-
-
-//         // ! *************************************
-//         $Map = $data['and'];
-//         $not = $data['not'];
-//         // ! *************************************
-
-//         $filtersOrders = PedidosShopify::with(['operadore.up_users','transportadora','users.vendedores','novedades','pedidoFecha','ruta','subRuta'])
-//             ->whereRaw("STR_TO_DATE(fecha_entrega, '%e/%c/%Y') BETWEEN ? AND ?", [$startDateFormatted, $endDateFormatted])
-//             ->where((function ($filtersOrders) use ($Map) {
-//                 foreach ($Map as $condition) {
-//                     foreach ($condition as $key => $valor) {
-//                         if (strpos($key, '.') !== false) {
-//                             $relacion = substr($key, 0, strpos($key, '.'));
-//                             $propiedad = substr($key, strpos($key, '.') + 1);
-//                             $this->recursiveWhereHas($filtersOrders, $relacion, $propiedad, $valor);
-//                         } else {
-//                             $filtersOrders->where($key, '=', $valor);
-//                         }
-
-//                     }
-//                 }
-//             }))->where((function ($filtersOrders) use ($not) {
-//             foreach ($not as $condition) {
-//                 foreach ($condition as $key => $valor) {
-//                     if (strpos($key, '.') !== false) {
-//                         $relacion = substr($key, 0, strpos($key, '.'));
-//                         $propiedad = substr($key, strpos($key, '.') + 1);
-//                         $this->recursiveWhereHas($filtersOrders, $relacion, $propiedad, $valor);
-//                     } else {
-//                         $filtersOrders->where($key, '!=', $valor);
-//                     }
-
-//                 }
-//             }
-//         }));
-
-
-//     if ($filtersOrders->isEmpty()) {
-//         return response()->json([
-//             'code' => 400,
-//             'Result' => 'No Existe Ningun Registro'
-//         ], 400);
-//     }
-
-//     // Transforma los resultados en un formato deseado (similar al formato del código original)
-//     $result = $filtersOrders->map(function ($objeto) {
-//         return [
-//             "Fecha de Ingreso" => $objeto->Marca_T_I,
-//             "Fecha de Entrega" => $objeto->Fecha_Entrega,
-//             // Agrega los otros campos que necesites
-//         ];
-//     });
-
-//     // Exporta los resultados a Excel (se asume que ya tienes configurada la exportación a Excel en Laravel)
-
-//     // Genera un nombre de archivo único
-//     $uniqueFileName = 'data_' . Str::random(6) . '.xlsx';
-
-//     // Ruta para almacenar el archivo
-//     $filePath = storage_path('public/uploads/') . $uniqueFileName;
-
-//     // Guarda los datos en el archivo Excel
-//     // (debes implementar la lógica de exportación a Excel en Laravel)
-
-//     // Crea un registro en la base de datos para el informe generado
-//     // GenerateReport::create([
-//     //     'fecha' => $fecha,
-//     //     'archivo' => $filePath,
-//     //     'id_master' => $idMaster
-//     // ]);
-
-//     // // Retorna la respuesta con la ubicación del archivo generado
-//     // return response()->json([
-//     //     'code' => 200,
-//     //     'Result' => $filePath
-//     // ], 200);
-// }
-
-
-    
-
--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPExcel' => array($vendorDir . '/phpoffice/phpexcel/Classes'),
-    'Maatwebsite\\Excel\\' => array($vendorDir . '/maatwebsite/excel/src'),
     'Laracasts\\Flash' => array($vendorDir . '/laracasts/flash/src'),
+    'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
 );
