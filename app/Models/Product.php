7483ac43a7e7209323f1fec1adbd38268ed7bd3c
Dianac <dcarlosamam@gmail.com>
2023-11-13 19:51:34 -0500

    draft productAPIController

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,42 +1,55 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ * 
+ * @property int $product_id
+ * @property string|null $product_name
+ * @property int|null $stock
+ * @property string|null $features
+ * @property float|null $price
+ * @property string|null $url_img
+ * @property int|null $warehouse_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * 
+ * @property Warehouse|null $warehouse
+ *
+ * @package App\Models
+ */
 class Product extends Model
 {
-    public $table = 'products';
+	protected $table = 'products';
+	protected $primaryKey = 'product_id';
 
-    public $fillable = [
-        'product_name',
-        'stock',
-        'features',
-        'price',
-        'url_img',
-        'warehouse_id'
-    ];
+	protected $casts = [
+		'stock' => 'int',
+		'price' => 'float',
+		'warehouse_id' => 'int'
+	];
 
-    protected $casts = [
-        'product_name' => 'string',
-        'features' => 'string',
-        'price' => 'decimal:2',
-        'url_img' => 'string'
-    ];
+	protected $fillable = [
+		'product_name',
+		'stock',
+		'features',
+		'price',
+		'url_img',
+		'warehouse_id'
+	];
 
-    public static array $rules = [
-        'product_name' => 'nullable|string|max:70',
-        'stock' => 'nullable',
-        'features' => 'nullable|string',
-        'price' => 'nullable|numeric',
-        'url_img' => 'nullable|string|max:70',
-        'warehouse_id' => 'nullable',
-        'created_at' => 'nullable',
-        'updated_at' => 'nullable'
-    ];
+	public function warehouse()
+	{
+        return $this->belongsTo(Warehouse::class, 'warehouse_id', 'warehouse_id');
 
-    public function warehouse(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(\App\Models\Warehouse::class, 'warehouse_id');
-    }
+	}
+	
 }
