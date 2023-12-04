5fdaf12dade779d5cf1ff27ddb527474dbd1e51d
Dianac <dcarlosamam@gmail.com>
2023-12-04 18:25:14 -0500

    new getproduct

--- app/Http/Controllers/API/ProductAPIController.php
+++ app/Http/Controllers/API/ProductAPIController.php
@@ -21,6 +21,96 @@ class ProductAPIController extends Controller
     }
 
 
+    // public function getProducts(Request $request)
+    // {
+    //     //all for catalog
+    //     $data = $request->json()->all();
+
+    //     $pageSize = $data['page_size'];
+    //     $pageNumber = $data['page_number'];
+    //     $searchTerm = $data['search'];
+
+    //     $populate = $data['populate'];
+    //     if ($searchTerm != "") {
+    //         $filteFields = $data['or'];
+    //     } else {
+    //         $filteFields = [];
+    //     }
+
+    //     $andMap = $data['and'];
+
+    //     $products = Product::with($populate)
+    //         ->where(function ($products) use ($searchTerm, $filteFields) {
+    //             foreach ($filteFields as $field) {
+    //                 if (strpos($field, '.') !== false) {
+    //                     $relacion = substr($field, 0, strpos($field, '.'));
+    //                     $propiedad = substr($field, strpos($field, '.') + 1);
+    //                     $this->recursiveWhereHas($products, $relacion, $propiedad, $searchTerm);
+    //                 } else {
+    //                     $products->orWhere($field, 'LIKE', '%' . $searchTerm . '%');
+    //                 }
+    //             }
+    //         })
+    //         ->where((function ($products) use ($andMap) {
+    //             foreach ($andMap as $condition) {
+    //                 foreach ($condition as $key => $valor) {
+    //                     if (strpos($key, '.') !== false) {
+    //                         $relacion = substr($key, 0, strpos($key, '.'));
+    //                         $propiedad = substr($key, strpos($key, '.') + 1);
+    //                         $this->recursiveWhereHas($products, $relacion, $propiedad, $valor);
+    //                     } else {
+    //                         $products->where($key, '=', $valor);
+    //                     }
+    //                 }
+    //             }
+    //         }))
+    //         ->whereHas('warehouse', function ($warehouse) {
+    //             $warehouse->where('active', 1)
+    //             ->where('approved', 1);
+    //         })
+    //         ->where('active', 1)//los No delete
+    //         ->where('approved', 1);
+    //     // ! sort
+    //     $orderByText = null;
+    //     $orderByDate = null;
+    //     $sort = $data['sort'];
+    //     $sortParts = explode(':', $sort);
+
+    //     $pt1 = $sortParts[0];
+
+    //     $type = (stripos($pt1, 'fecha') !== false || stripos($pt1, 'marca') !== false) ? 'date' : 'text';
+
+    //     $dataSort = [
+    //         [
+    //             'field' => $sortParts[0],
+    //             'type' => $type,
+    //             'direction' => $sortParts[1],
+    //         ],
+    //     ];
+
+    //     foreach ($dataSort as $value) {
+    //         $field = $value['field'];
+    //         $direction = $value['direction'];
+    //         $type = $value['type'];
+
+    //         if ($type === "text") {
+    //             $orderByText = [$field => $direction];
+    //         } else {
+    //             $orderByDate = [$field => $direction];
+    //         }
+    //     }
+
+    //     if ($orderByText !== null) {
+    //         $products->orderBy(key($orderByText), reset($orderByText));
+    //     } else {
+    //         $products->orderBy(DB::raw("STR_TO_DATE(" . key($orderByDate) . ", '%e/%c/%Y')"), reset($orderByDate));
+    //     }
+    //     // ! **************************************************
+    //     $products = $products->paginate($pageSize, ['*'], 'page', $pageNumber);
+    //     return response()->json($products);
+    // }
+
+
     public function getProducts(Request $request)
     {
         //all for catalog
@@ -31,6 +121,16 @@ class ProductAPIController extends Controller
         $searchTerm = $data['search'];
 
         $populate = $data['populate'];
+        $outFilters = $data['out_filters'] ?? [];
+
+        // Asumiendo que las categorías vienen en un filtro llamado 'categories' dentro de 'out_filters'
+        $categoryFilters = [];
+        foreach ($outFilters as $filter) {
+            if (isset($filter['input_categories'])) {
+                $categoryFilters = $filter['input_categories'];
+                break;
+            }
+        }
         if ($searchTerm != "") {
             $filteFields = $data['or'];
         } else {
@@ -66,10 +166,36 @@ class ProductAPIController extends Controller
             }))
             ->whereHas('warehouse', function ($warehouse) {
                 $warehouse->where('active', 1)
-                ->where('approved', 1);
+                    ->where('approved', 1);
             })
-            ->where('active', 1)//los No delete
-            ->where('approved', 1);
+            ->where('active', 1) //los No delete
+            ->where('approved', 1)
+            ->when(isset($data['out_filters']), function ($query) use ($data) {
+                foreach ($data['out_filters'] as $filter) {
+                    foreach ($filter as $key => $value) {
+                        if ($key === 'price_range') {
+                            $priceRange = explode('-', $value);
+                            $minPrice = isset($priceRange[0]) && $priceRange[0] !== '' ? floatval($priceRange[0]) : null;
+                            $maxPrice = isset($priceRange[1]) && $priceRange[1] !== '' ? floatval($priceRange[1]) : null;
+
+                            if (!is_null($minPrice) && !is_null($maxPrice)) {
+                                $query->whereBetween('price', [$minPrice, $maxPrice]);
+                            } elseif (!is_null($minPrice)) {
+                                $query->where('price', '>=', $minPrice);
+                            } elseif (!is_null($maxPrice)) {
+                                $query->where('price', '<=', $maxPrice);
+                            }
+                        }
+                    }
+                }
+            })
+            ->when(count($categoryFilters) > 0, function ($query) use ($categoryFilters) {
+                $query->where(function ($query) use ($categoryFilters) {
+                    foreach ($categoryFilters as $category) {
+                        $query->orWhereRaw("JSON_CONTAINS(JSON_EXTRACT(features, '$[*].categories'), '\"$category\"')");
+                    }
+                });
+            });            
         // ! sort
         $orderByText = null;
         $orderByDate = null;
@@ -105,8 +231,9 @@ class ProductAPIController extends Controller
         } else {
             $products->orderBy(DB::raw("STR_TO_DATE(" . key($orderByDate) . ", '%e/%c/%Y')"), reset($orderByDate));
         }
-        // ! **************************************************
+        // ! ******
         $products = $products->paginate($pageSize, ['*'], 'page', $pageNumber);
+
         return response()->json($products);
     }
 
