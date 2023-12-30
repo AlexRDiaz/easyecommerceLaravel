14e4e48ade09572958897f4bc3b201d9f317ecc4
AlexRDiaz <bugi2532@hotmail.com>
2023-12-30 11:43:54 -0500

    addded shopify webhook

--- app/Http/Controllers/ShopifyWebhookController.php
+++ app/Http/Controllers/ShopifyWebhookController.php
@@ -42,24 +42,24 @@ class ShopifyWebhookController extends Controller
         // Si el webhook no es el esperado, responde con un código de estado 404 (no encontrado)
         return response()->json(['error' => 'Webhook no válido'], 404);
     }
+    function verifyWebhook($data, $hmacHeader)
+{
+    $calculatedHmac = base64_encode(hash_hmac('sha256', $data, "5dd4d59dc579a8bc4972383c42be5b7b", true));
+    return hash_equals($calculatedHmac, $hmacHeader);
+}
     public function handleShopRedact(Request $request)
     {
-        // Verifica si el webhook es válido (puedes implementar lógica adicional para validar el webhook)
-        $shopifySecret = '5dd4d59dc579a8bc4972383c42be5b7b'; // Tu secreto de aplicación Shopify
-
         $hmacHeader = $request->header('X-Shopify-Hmac-SHA256');
-        $data = $request->getContent();
+        $data = file_get_contents('php://input');
 
-        // Calcula el HMAC usando tu secreto
-        $calculatedHmac = base64_encode(hash_hmac('sha256', $data, $shopifySecret, true));
+        $verified = $this->verifyWebhook($data, $hmacHeader);
 
-        // Compara el HMAC proporcionado con el calculado
-        if (hash_equals($hmacHeader, $calculatedHmac)) {
-            // La firma es válida, el webhook es auténtico
-            // ... tu lógica de manejo del webhook ...
-            return response()->json(['message' => 'Webhook recibido'], 200);
+        if ($verified) {
+            // El webhook es auténtico, procesar el payload
+            // ...
+            return response()->json(['message' => 'Webhook verificado'], 200);
         } else {
-            // La firma no coincide, el webhook podría no ser auténtico
+            // El webhook no es auténtico, responder con un código de estado 401
             return response()->json(['error' => 'No autorizado'], 401);
         }
     }
